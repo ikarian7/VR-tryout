306ea2badcc8474dd659af50518951508ae32c2b
ikarian7 <scott.marchant7@gmail.com>
2022-01-17 11:04:27 +0100

    passthrough

---
+++ Assets/PassthroughManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PassthroughManager : MonoBehaviour
+{
+
+    public OVRPassthroughLayer passthrough;
+    public OVRInput.Button button;
+    public OVRInput.Controller controller;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(OVRInput.GetDown(button, controller))
+        {
+            passthrough.hidden = !passthrough.hidden;
+        }
+    }
+}
